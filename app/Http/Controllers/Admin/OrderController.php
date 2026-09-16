72ed342b1db843281d88ded45da8b643d70d3e2e
guerrero-27 <markkevinguerrero7@gmail.com>
2026-09-16 21:49:03 +0800

    adding admin orders logic

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -10,7 +10,7 @@ class OrderController extends Controller
 {
     public function index(Request $request)
     {
-        $query = Order::with('user');
+        $query = Order::with('user')->withCount('items');
 
         if ($request->filled('status')) {
             $query->where('status', $request->status);
